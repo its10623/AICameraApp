48325e1a1a652426c19269425f11f31f5735ba59
its10623 <its10623@gmail.com>
2026-08-05 23:58:14 +0900

    feat: bindUserRepository 바인드 추가

--- app/src/main/java/com/smoothsm/cameraapp/data/di/RepositoryModule.kt
+++ app/src/main/java/com/smoothsm/cameraapp/data/di/RepositoryModule.kt
@@ -2,8 +2,10 @@ package com.smoothsm.cameraapp.data.di
 
 import com.smoothsm.cameraapp.data.repository.CameraRepositoryImpl
 import com.smoothsm.cameraapp.data.repository.ScanRepositoryImpl
+import com.smoothsm.cameraapp.data.repository.UserRepositoryImpl
 import com.smoothsm.cameraapp.domain.repository.CameraRepository
 import com.smoothsm.cameraapp.domain.repository.ScanRepository
+import com.smoothsm.cameraapp.domain.repository.UserRepository
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -20,4 +22,8 @@ abstract class RepositoryModule {
     @Binds
     @Singleton
     abstract fun bindScanRepository(impl: ScanRepositoryImpl): ScanRepository
+
+    @Binds
+    @Singleton
+    abstract fun bindUserRepository(impl: UserRepositoryImpl): UserRepository
 }
