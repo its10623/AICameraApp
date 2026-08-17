72f13de3200dbf36c4e04928716505b5fef15870
its10623 <its10623@gmail.com>
2026-08-17 23:15:09 +0900

    build: 핫스완 컴파일러 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
     alias(libs.plugins.hilt.android)
     alias(libs.plugins.ktlint)
     alias(libs.plugins.firebaseSdk)
+    alias(libs.plugins.hotswan.compiler)
 }
 
 android {
--- build.gradle.kts
+++ build.gradle.kts
@@ -5,4 +5,5 @@ plugins {
     alias(libs.plugins.ksp)                  apply false
     alias(libs.plugins.hilt.android)         apply false
     alias(libs.plugins.ktlint)               apply false
+    alias(libs.plugins.hotswan.compiler)     apply false
 }
